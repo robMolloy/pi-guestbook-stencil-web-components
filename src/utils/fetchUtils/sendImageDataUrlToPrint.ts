8355ad93356475542d41be68f59f45d903afa044
robMolloy <robmolloy@hotmail.co.uk>
2024-01-04 09:57:25 +0000

    setup retrieveBackupImages route

--- src/utils/fetchUtils/sendImageDataUrlToPrint.ts
+++ src/utils/fetchUtils/sendImageDataUrlToPrint.ts
@@ -34,20 +34,9 @@ export const sendImageDataUrlsForBackup = async (p: { imageDataUrls: string[] })
   }
 };
 
-export const retrieveBackupImageDataUrls = async (p: { imageDataUrls: string[] }) => {
-  try {
-    const controller = new AbortController();
-    setTimeout(() => controller.abort(), 3000);
-    const resp = await fetch(`${localStorage.getItem('serverBaseUrl')}${localStorage.getItem('serverBackupImagesEndpoint')}`, {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ imageDataUrls: p.imageDataUrls }),
-      signal: controller.signal,
-    });
-    const json = await resp.json();
-    if (json.success === true) return { success: true } as const;
-    return { success: false, error: 'response invalid' } as const;
-  } catch (error) {
-    return { success: false, error: error.name } as const;
-  }
+export const retrieveBackupImageDataUrls = async () => {
+  console.log('images');
+};
+export const retrieveBackupImageListUrls = async () => {
+  console.log('list');
 };
